efc20cb586418fddb57f6e8aaf9127fc0ddb824f
P-Schlichting <paskal.schlichting@gmx.de>
2022-09-30 13:22:41 +0200

    Update

---
+++ Aufgabe1_KW39.py
@@ -0,0 +1,14 @@
+x = 5
+y = 10
+z = 10
+print(x < y)                        #True
+print(y < z)                        #False
+print(x == 5)                       #True
+print(not x == 5)                   #False
+print(x != 5)                       #False
+print(not x != 5)                   #True   
+print(x == "5")                     #False
+print(5 == x + 0.00000000001)       #False
+print(x == 5 and y == 10)           #True
+print(x == 5 and y == 5)            #False
+print(x == 5 or y == 5)             #True
